45d5e90dd3e429fbac0bf048423bf948b728114d
RogueMissile <166340198+RogueMissile@users.noreply.github.com>
2025-06-16 10:21:45 +1200

    0.2
    Added a function to find the NS and EW components of the distance between the two points. This in the future could be used to find error in the x and y directions on the drone.

--- Drone-Rescue/pythonprototype.py
+++ Drone-Rescue/pythonprototype.py
@@ -1,24 +1,25 @@
 from numpy import cos, sin, arctan2, sqrt
-import math
+from math import pi
 
-# Radius of the earth
+
+# Radius of the earth 
 r = 6371.2
 
 # Start point
-latitude = 51.5007
-longitude = 0.1246
+latitude = 39.0925
+longitude = 174.1120
 
 # End point
-latitude2 = 40.6892
-longitude2 = 74.0445
+latitude2 = 39.0930
+longitude2 = 174.1120
 
- # Convert angles to radians
-latituder = latitude * (math.pi / 180)
-longituder = longitude * (math.pi / 180)
-latitude2r = latitude2 * (math.pi / 180)
-longitude2r = longitude2 * (math.pi / 180)
+# Convert angles to radians
+latituder = latitude * (pi / 180)
+longituder = longitude * (pi / 180)
+latitude2r = latitude2 * (pi / 180)
+longitude2r = longitude2 * (pi / 180)
 
-def haversine_distance(lon1,lat1,lon2,lat2,rad):
+def total_distance(lon1,lat1,lon2,lat2,r):
 
     # Get delta latitude and delta longitude
     dlon = lon2 - lon1
@@ -27,9 +28,22 @@ def haversine_distance(lon1,lat1,lon2,lat2,rad):
     # Apply haversine formula
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * arctan2(sqrt(a), sqrt(1 - a))
-    distance = rad * c * 1000
+    distance = r * c * 1000  # Convert to meters
     
     return distance
 
+def ns_ew_distance(lon1,lat1,lon2,lat2,r):
 
-print("Distance (m) =",haversine_distance(longituder,latituder,longitude2r,latitude2r,r))
+    # For North-South distance is just difference in latitude
+    ns = r * (lat2 - lat1) * 1000  # Convert to meters
+
+    # For East-West distance depends on the difference in longitude but must be scaled by 
+    # the cosine of latitude as the distance between longitude lines decreases as you move 
+    # toward the poles
+    ew = r * (lon2 - lon1) * cos((lat1 + lat2) / 2) * 1000 # Convert to meters
+
+    return ns,ew
+
+print("Total Distance (m) =",total_distance(longituder,latituder,longitude2r,latitude2r,r))
+print("NS Component =",ns_ew_distance(longituder,latituder,longitude2r,latitude2r,r)[0])
+print("EW Component =",ns_ew_distance(longituder,latituder,longitude2r,latitude2r,r)[1])
